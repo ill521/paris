336246e4bd13f90a8f29e19e705b60388eb08b0d
ill521 <83938020+ill521@users.noreply.github.com>
2021-07-16 18:05:34 +0900

    paris-1-8
    product부분 슬릭까지 완료

---
+++ js/sliderA.js
@@ -0,0 +1,9 @@
+$('.sliderA').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    prevArrow: $('#arrow_prev'),
+    nextArrow: $('#arrow_next'),
+  });
---
+++ js/sliderB.js
@@ -0,0 +1,8 @@
+$('.sliderB').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('#arrow_prev3'),
+    nextArrow: $('#arrow_next3'),
+  });
